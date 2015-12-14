87e6eb478e21977deca35db8b7e28b0ef52550df
bobha <bobha@DESKTOP-L9J6TUG>
2015-12-14 22:35:36 +0000

    update to PropDetailsBean-> providing a constructor that takes 4 strings.. from the drop down lists.

--- src/graft/ie/PropDetailsBean.java
+++ src/graft/ie/PropDetailsBean.java
@@ -1,16 +1,20 @@
 package graft.ie;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class PropDetailsBean {
+public class PropDetailsBean implements Serializable{
 	
 	Image masterPropImage;
 	ArrayList<ImageView> masterPropImageViewArray;
 	String agencyString = "none given";
 	Double priceDouble;
+	String priceRangeString;
+	String propTypeString;
+	String locationString;
 	
 	public PropDetailsBean(Double priceDouble) {
 		super();
@@ -18,6 +22,14 @@ public class PropDetailsBean {
 		this.masterPropImageViewArray = new ArrayList<ImageView>();
 		Image tempImage = new Image("image/blue-home-icon.png");
 		setMasterPropImageView(tempImage);
+	}
+	
+	public PropDetailsBean(String propType, String location, String agency, String priceRange){
+		setPropTypeString(propType);
+		setLocationString(location);
+		setAgencyString(agency);
+		setPriceRangeString(priceRange);
+		
 	}
 	
 	public PropDetailsBean(Image masterPropImage, Double priceDouble) {
@@ -87,6 +99,30 @@ public class PropDetailsBean {
 	}
 	public void setPriceDouble(Double priceDouble) {
 		this.priceDouble = priceDouble;
+	}
+
+	public String getPriceRangeString() {
+		return priceRangeString;
+	}
+
+	public void setPriceRangeString(String priceRangeString) {
+		this.priceRangeString = priceRangeString;
+	}
+
+	public String getPropTypeString() {
+		return propTypeString;
+	}
+
+	public void setPropTypeString(String propTypeString) {
+		this.propTypeString = propTypeString;
+	}
+
+	public String getLocationString() {
+		return locationString;
+	}
+
+	public void setLocationString(String locationString) {
+		this.locationString = locationString;
 	}
 	
 	
